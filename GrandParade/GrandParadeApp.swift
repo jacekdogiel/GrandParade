3b9ce68a1e2a037eba3c2ad7494bc486b50d97ac
Jacek <jacek.dogiel@gmail.com>
2023-03-07 21:21:43 +0100

    Initial Commit

---
+++ GrandParade/GrandParadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrandParadeApp.swift
+//  GrandParade
+//
+//  Created by Jacek Dogiel on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GrandParadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
